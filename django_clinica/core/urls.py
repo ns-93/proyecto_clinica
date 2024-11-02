e5e23916f41741cc7551dafbc773cdd75bb11da8
ns-93 <emiliosalazar062@gmail.com>
2024-11-02 05:29:01 -0300

    creacion de vistas
    creacion de alguna vistas de prueba y modelacion inicial de los html

--- django_clinica/core/urls.py
+++ django_clinica/core/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import HomeView
+from .views import HomeView, PricingView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
+    path('pricing/', PricingView.as_view(),name='pricing'),
     
 ]
--- django_clinica/core/views.py
+++ django_clinica/core/views.py
@@ -6,3 +6,7 @@ from django.views.generic import TemplateView
 
 class HomeView(TemplateView):
     template_name= 'home.html'
+
+
+class PricingView(TemplateView):
+    template_name= 'pricing.html'
